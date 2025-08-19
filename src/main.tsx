56a86dd0b619ab6dff08305c7fceefe01c0d6e55
dj1paolo <paoquiamzon@gmail.com>
2025-08-19 09:40:48 -0700

    fix: center #quote-box and load Mixtape.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+
+
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./Mixtape.css"; // or "./index.css" — whichever you actually use
 
 createRoot(document.getElementById("root")!).render(
